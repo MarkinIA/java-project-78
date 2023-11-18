c41d77b48ffe150da9de022fc7532297512c2bb8
Markin Ilya <markin.il86@gmail.com>
2023-11-18 14:05:36 +0300

    First Commmit

---
+++ app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    jacoco
+    application
+    checkstyle
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.47.0"
+}
+application {mainClass.set("hexlet.code.App")}
+
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.19.0")
+    implementation("info.picocli:picocli:4.7.5")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.15.3")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.2")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.5")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
