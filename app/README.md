3ad4bede33585c7aefee4f4ee5457468c81c80ba
Markin Ilya <markin.il86@gmail.com>
2023-11-22 21:36:04 +0300

    Hard Stuck cause stupid

--- app/README.md
+++ app/README.md
@@ -1,1 +1,8 @@
+[![Maintainability](https://api.codeclimate.com/v1/badges/08f61c3e134ea7929b0b/maintainability)](https://codeclimate.com/github/MarkinIA/java-project-78/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/08f61c3e134ea7929b0b/test_coverage)](https://codeclimate.com/github/MarkinIA/java-project-78/test_coverage)
 ![example workflow](https://github.com/MarkinIA/java-project-78/actions/workflows/main.yml/badge.svg)
+
+Скажу сразу, что мне ужасно стыдно за то, что я написал, но в силу своей недальновидности пока не могу придумать ничего лучше
+Из вариантов для улучшения, которые я рассматривал:
+1) В классе BaseSchema сделать метод для валидации, который покрывает все возможные типы даных, однако это никак не спасет от переиспользования кода
+2) 
--- app/src/test/java/AppTest.java
+++ app/src/test/java/AppTest.java
@@ -62,7 +62,7 @@ public class AppTest {
     }
 
     @Test
-    void testMapSchema() throws Exception {
+    void testMapSchema() {
         Validator v = new Validator();
         MapSchema schema = v.map();
         assertThat(schema.isValid(null)).isTrue(); // true
@@ -91,11 +91,28 @@ public class AppTest {
         Map<String, BaseSchema> schemas = new HashMap<>();
 
         schemas.put("name", v.string().required());
+        schemas.put("age", v.number().positive());
 
         schema.shape(schemas);
 
         Map<String, Object> human1 = new HashMap<>();
-        human1.put("name", "");
-        assertThat(schema.isValid(human1)).isFalse(); // true
+        human1.put("name", "Kolya");
+        human1.put("age", 100);
+        assertThat(schema.isValid(human1)).isTrue(); // true
+
+        Map<String, Object> human2 = new HashMap<>();
+        human2.put("name", "Maya");
+        human2.put("age", null);
+        assertThat(schema.isValid(human2)).isTrue();
+
+        Map<String, Object> human3 = new HashMap<>();
+        human3.put("name", "");
+        human3.put("age", null);
+        assertThat(schema.isValid(human3)).isFalse();
+
+        Map<String, Object> human4 = new HashMap<>();
+        human4.put("name", "Valya");
+        human4.put("age", -5);
+        assertThat(schema.isValid(human4)).isFalse();
     }
 }
