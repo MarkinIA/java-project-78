5b8c0a2f66d3f3acba1c4e4769a38eb435635751
Markin Ilya <markin.il86@gmail.com>
2023-11-24 22:08:13 +0300

    Fixes

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -24,8 +24,8 @@ public class MapSchema extends BaseSchema {
     public MapSchema sizeof(int num) {
         validationRules.add(m -> {
             //if (!Objects.isNull(m) && m instanceof Map) {
-                Map<String, Object> map = new HashMap<>((Map<? extends String, ?>) m);
-                return map.size() == num;
+            Map<String, Object> map = new HashMap<>((Map<? extends String, ?>) m);
+            return map.size() == num;
             //}
             //return true;
         });
