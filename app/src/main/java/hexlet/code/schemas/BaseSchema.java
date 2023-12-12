507938182384d9ecae8c9d0cb33ef8f42df0077d
Markin Ilya <markin.il86@gmail.com>
2023-12-12 16:34:11 +0300

    Fixes

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -5,10 +5,11 @@ import java.util.List;
 import java.util.Objects;
 import java.util.function.Predicate;
 
-public abstract class BaseSchema {
-    public List<Predicate<Object>> validationRules = new ArrayList<>();
-    public boolean isRequired = false;
-    public boolean isValid(Object obj) {
+public class BaseSchema {
+    protected List<Predicate<Object>> validationRules = new ArrayList<>();
+    protected boolean isRequired = false;
+
+    public final boolean isValid(Object obj) {
         if (isRequired || !Objects.isNull(obj)) {
             for (Predicate<Object> validation : validationRules) {
                 if (!validation.test(obj)) {
